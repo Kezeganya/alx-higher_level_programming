347b745f9f34afc4692ac4cce7f1fe6dfd788a84
kelvin ezeganya <kenteezy2014@gmail.com>
2024-04-04 18:27:04 -0700

    add files

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,31 +1,10 @@
 #!/usr/bin/python3
-# 2-read_lines.py
+# 2-to_json_string.py
 # Kelvin Ezeganya
-"""Defines a text file-reading function."""
+"""Defines a string-to-JSON function."""
+import json
 
 
-def read_lines(filename="", nb_lines=0):
-    """Print a given number of lines from a UTF8 text file to stdout.
-
-    Args:
-        filename (str): The name of the file.
-        nb_lines (int): The number of lines to read from the file.
-    """
-    with open(filename, encoding="utf-8") as f:
-        if nb_lines <= 0:
-            print(f.read(), end="")
-            return
-
-        lines = 0
-        for line in f:
-            lines += 1
-        f.seek(0)
-        if nb_lines >= lines:
-            print(f.read(), end="")
-            return
-
-        else:
-            n = 0
-            while n < nb_lines:
-                print(f.readline(), end="")
-                n += 1
+def to_json_string(my_obj):
+    """Return the JSON representation of a string object."""
+    return json.dumps(my_obj)
